43fba0edc06212b33783257ce4a6842030824504
JuandCaro <juancho04302@gmail.com>
2025-10-02 21:07:04 -0500

    Versión 1.4 Funcional

---
+++ scriptfinanci_1.4.sql
@@ -0,0 +1,212 @@
+DROP TABLE IF EXISTS tab_terceros;
+DROP TABLE IF EXISTS tab_val_ind;
+DROP TABLE IF EXISTS tab_indicadores;
+DROP TABLE IF EXISTS tab_activos_fijos;
+DROP TABLE IF EXISTS tab_proyectos;
+DROP TABLE IF EXISTS tab_presupuesto;
+DROP TABLE IF EXISTS tab_inversores;
+DROP TABLE IF EXISTS tab_ciudades;
+DROP TABLE IF EXISTS tab_deptos;
+DROP TABLE IF EXISTS tab_pmtros_financieros;
+DROP TABLE IF EXISTS tab_pmtros_generales;
+DROP TYPE  IF EXISTS audit_trail;
+
+--TIPE PARA USUARIOS
+
+CREATE TYPE audit_trail AS
+(
+    usr_insert  VARCHAR,
+    fec_insert  TIMESTAMP WITHOUT TIME ZONE,
+    usr_update  VARCHAR,
+    fec_update  TIMESTAMP WITHOUT TIME ZONE
+);
+
+
+--TABLA DE PARÁMERTROS GENERALES
+
+CREATE TABLE IF NOT EXISTS tab_pmtros_generales
+(
+    id_empresa     VARCHAR         NOT NULL              CHECK  (LENGTH (id_empresa) = 10),             
+    nom_empresa    VARCHAR         NOT NULL              CHECK  (LENGTH(nom_empresa) >=5 AND LENGTH(nom_empresa) <=60),
+    dir_empresa    VARCHAR         NOT NULL,             
+    tel_empresa    DECIMAL (10,0)  NOT NULL              CHECK  (tel_empresa >=0),
+    email_empresa  VARCHAR         NOT NULL,  
+    val_poriva     DECIMAL (2,0)   NOT NULL DEFAULT 0    CHECK  (val_poriva   >=0    AND val_poriva    < 100),
+    val_porrete    DECIMAL (2,0)   NOT NULL DEFAULT 0    CHECK  (val_porrete  >=0    AND val_porrete   < 100),
+    val_reteica    DECIMAL (2,0)   NOT NULL DEFAULT 0    CHECK  (val_reteica  >=0    AND val_reteica   < 100),
+    val_porutil    DECIMAL (3,0)   NOT NULL DEFAULT 0    CHECK  (val_porutil  >=0    AND val_porutil   < 1000),
+    val_longitud   DECIMAL (12,10) NOT NULL              CHECK  (val_longitud >=-4   AND val_longitud  <=80),
+    val_latitud    DECIMAL (12,10) NOT NULL              CHECK  (val_latitud  >=-80  AND val_latitud   <=-50),
+    ind_autorrete  BOOLEAN         NOT NULL,             --TRUE = autorrete / FALSE = no autorrete
+    PRIMARY KEY (id_empresa)
+
+);
+
+--TABLA DE PARÁMETROS FINANCI
+
+CREATE TABLE IF NOT EXISTS tab_pmtros_financieros
+(   
+    id_pmtro_financiero   VARCHAR                       NOT NULL,
+	pasivos_corrientes    DECIMAL   (18,2)              NOT NULL,
+	activos_corrientes    DECIMAL   (18,2)              NOT NULL,
+	inventario            DECIMAL   (18,2)              NOT NULL,
+	efectivo              DECIMAL   (18,2)              NOT NULL,
+	equiv_efectivo        DECIMAL   (18,2)              NOT NULL,
+	patrimonio            DECIMAL   (18,2)              NOT NULL,
+	interes               DECIMAL   (18,2)              NOT NULL,
+	utilidad_neta         DECIMAL   (18,2)              NOT NULL,
+	utilidad_operativa    DECIMAL   (18,2)              NOT NULL,
+	ventas                DECIMAL   (18,2)              NOT NULL,
+	costo_de_ventas       DECIMAL   (18,2)              NOT NULL,
+	capital_invertido     DECIMAL   (18,2)              NOT NULL,
+	equivalentes          DECIMAL   (18,2)              NOT NULL,
+	pasivo_financiero     DECIMAL   (18,2)              NOT NULL,
+	cxc_promedio          DECIMAL   (18,2)              NOT NULL,
+	ventas_credito        DECIMAL   (18,2)              NOT NULL,
+	inv_promedio          DECIMAL   (18,2)              NOT NULL,
+	compras_credito       DECIMAL   (18,2)              NOT NULL,
+	cxp_promedio          DECIMAL   (18,2)              NOT NULL,
+    PRIMARY KEY (id_pmtro_financiero)
+);
+ 
+--TABLA DE DEPARTAMENTOS
+
+CREATE TABLE IF NOT EXISTS tab_deptos
+(
+    id_depto       		  VARCHAR                       NOT NULL    CHECK   (LENGTH(id_depto)=2),
+    nom_depto      		  VARCHAR                       NOT NULL    CHECK   (LENGTH(nom_depto) <=30),
+    datos_audit           AUDIT_TRAIL,
+    PRIMARY KEY (id_depto)
+);
+
+--TABLA DE CIUDADES
+
+CREATE TABLE IF NOT EXISTS tab_ciudades
+(
+    id_ciudad             VARCHAR                       NOT NULL    CHECK   (LENGTH (id_ciudad) =5),
+    id_depto       		  VARCHAR                       NOT NULL    CHECK   (LENGTH(id_depto)=2),
+	nom_ciudad            VARCHAR                       NOT NULL    CHECK   (LENGTH (nom_ciudad) <=30),
+    ind_capital           BOOLEAN                       NOT NULL,    --TRUE = es capital / FALSE = no es capital
+    cod_postal            VARCHAR                       NOT NULL    CHECK   (LENGTH (cod_postal) =6),
+    val_longitud          DECIMAL   (12,10)             NOT NULL    CHECK   (val_longitud >=-4              AND val_longitud  <=80),
+    val_latitud           DECIMAL   (12,10)             NOT NULL    CHECK   (val_latitud  >=-80             AND val_latitud   <=-50),
+    datos_audit           AUDIT_TRAIL,
+    PRIMARY KEY (id_ciudad),
+	FOREIGN KEY (id_depto) REFERENCES tab_deptos(id_depto)
+);
+
+--TABLA DE INVERSORES
+
+CREATE TABLE IF NOT EXISTS tab_inversores
+(
+    id_inversor           VARCHAR                       NOT NULL    CHECK   (LENGTH (id_inversor) =10),
+    id_ciudad             VARCHAR                       NOT NULL    CHECK   (LENGTH (id_ciudad) =5),
+	nom_inversor          VARCHAR                       NOT NULL    CHECK   (LENGTH(nom_inversor) >=5           AND LENGTH(nom_inversor) <=60),
+    tipo_inversor         BOOLEAN                       NOT NULL,   --true = Natural / false = Jurdíco 
+    tel_inversor          DECIMAL   (10,0)              NOT NULL    CHECK   (tel_inversor >=0),
+    email_inversor        VARCHAR                       NOT NULL    CHECK   (LENGTH (email_inversor) <=120),   
+    por_participacion     DECIMAL   (3,1)               NOT NULL    CHECK   (por_participacion BETWEEN 0 AND 100),   
+    dir_inversor          VARCHAR                       NOT NULL    CHECK   (LENGTH (dir_inversor) <255),
+    val_long        	  DECIMAL	(9,6)               NOT NULL    CHECK (val_long BETWEEN -80   AND -67),
+    val_lat         	  DECIMAL	(9,6)               NOT NULL    CHECK (val_lat  BETWEEN -4 AND 12),       
+    datos_audit           AUDIT_TRAIL,
+    PRIMARY KEY (id_inversor),
+	FOREIGN KEY (id_ciudad) REFERENCES tab_ciudades(id_ciudad)
+);
+
+--TABLA DE PRESUPUESTO
+
+CREATE TABLE IF NOT EXISTS tab_presupuesto
+(
+    id_presupuesto        VARCHAR                       NOT NULL     CHECK  (LENGTH (id_presupuesto) >=1        AND LENGTH (id_presupuesto) <=10),
+    nom_presupuesto       VARCHAR                       NOT NULL     CHECK  (LENGTH (nom_presupuesto) <255),
+    monto_presupuesto     DECIMAL   (8,0)               NOT NULL     CHECK  (monto_presupuesto >= 0),
+    fec_inicio            DATE                          NOT NULL     CHECK  (fec_inicio >  '2025/01/1' 	    AND   fec_inicio < '2025/12/31'),
+    fec_fin               DATE                          NOT NULL     CHECK  (fec_fin    <  '2025/12/31' 	AND   fec_fin	 > '2025/01/1'),
+    descripcion           TEXT                          NOT NULL,
+    datos_audit           AUDIT_TRAIL,
+    PRIMARY KEY (id_presupuesto)
+);
+
+--TABLA DE PROYECTOS
+ 
+CREATE TABLE IF NOT EXISTS tab_proyectos
+(
+    id_proyecto	            VARCHAR                       NOT NULL      CHECK  (LENGTH (id_proyecto) >=1         AND LENGTH(id_proyecto) <=10),
+    id_inversor         	VARCHAR                       NOT NULL      CHECK  (LENGTH (id_inversor) =10),
+    id_presupuesto      	VARCHAR                       NOT NULL      CHECK  (LENGTH (id_presupuesto) >=1     	AND LENGTH (id_presupuesto) <=10),
+    nom_proyecto	        VARCHAR                       NOT NULL      CHECK  (LENGTH (nom_proyecto) <255),
+    descripcion	            TEXT                          NOT NULL,     --CHECK  (),
+    fec_inicio              DATE                          NOT NULL      CHECK  (fec_inicio >  '2025/01/1' 	    AND   fec_inicio < '2025/12/31'),
+    fec_fin                 DATE                          NOT NULL      CHECK  (fec_fin    <  '2025/12/31' 	    AND   fec_fin	 > '2025/01/1'),
+    presupuesto_asignado	DECIMAL(8,0)                  NOT NULL      CHECK  (presupuesto_asignado BETWEEN 0  AND  99999999),
+    resp_proyecto	        VARCHAR                       NOT NULL      CHECK  (LENGTH(resp_proyecto) >=5       		AND LENGTH(resp_proyecto) <=60),
+    datos_audit           AUDIT_TRAIL,
+    PRIMARY KEY (id_proyecto),
+    FOREIGN KEY (id_inversor)    REFERENCES tab_inversores(id_inversor),
+    FOREIGN KEY (id_presupuesto) REFERENCES tab_presupuesto(id_presupuesto)
+);
+
+--TABLA DE ACTIVOS FIJOS
+
+CREATE TABLE IF NOT EXISTS tab_activos_fijos
+(
+    id_activo	            VARCHAR                       NOT NULL       CHECK (LENGTH (id_activo) >=0 AND LENGTH (id_activo) <=100),       
+    nom_activo	            VARCHAR                       NOT NULL       CHECK (LENGTH(nom_activo) <255),
+    tip_activo	            BOOLEAN                       NOT NULL,      -- True = tangible     AND     false = intangible
+    val_inicial         	DECIMAL   (8,0)               NOT NULL       CHECK (val_inicial >=0  AND val_inicial <=99999999),     
+    val_contable	        DECIMAL   (8,0)               NOT NULL       CHECK (val_contable >=0  AND val_contable <=99999999),
+    val_residual	        DECIMAL   (8,0)               NOT NULL       CHECK (val_residual >=0  AND val_residual <=99999999),
+    ubicacion	            VARCHAR                       NOT NULL       CHECK (LENGTH (ubicacion) <=100),
+    datos_audit           AUDIT_TRAIL,
+    PRIMARY KEY (id_activo)
+);
+
+
+--TABLA DE INDICADORES
+
+CREATE TABLE IF NOT EXISTS tab_indicadores
+(
+    id_ind	                VARCHAR                       NOT NULL       CHECK (LENGTH (id_ind) <=50),
+    id_activo	            VARCHAR                       NOT NULL       CHECK (LENGTH (id_activo) >=0 AND LENGTH (id_activo) <=100),
+    descripcion	            TEXT                          NOT NULL,
+    tipo	                VARCHAR                       NOT NULL       CHECK (tipo IN ('Rentabilidad','Liquidez','Endeudamiento','Rotación')),
+    formula	                VARCHAR                       NOT NULL       CHECK (LENGTH(formula) <=255),
+    lim_alerta	            DECIMAL(2,0)                  NOT NULL       CHECK (lim_alerta BETWEEN 0 AND 99),  
+    datos_audit           AUDIT_TRAIL,
+    PRIMARY KEY (id_ind),
+    FOREIGN KEY (id_activo)    REFERENCES tab_activos_fijos(id_activo)
+);
+
+
+
+--TABLA DE TERCEROS
+
+CREATE TABLE IF NOT EXISTS tab_terceros
+(
+    id_tercero	            VARCHAR                        NOT NULL       CHECK (LENGTH (id_tercero) =10),
+    fec_nacimiento	        DATE                           NOT NULL,
+    nom_tercero	            VARCHAR                        NOT NULL       CHECK (LENGTH (nom_tercero) >=30  AND LENGTH(nom_tercero) <=60),
+    tel_tercero	            DECIMAL(10,0)                  NOT NULL       CHECK (tel_tercero >=0 ),
+    mail_tercero	        VARCHAR                        NOT NULL       CHECK (LENGTH(mail_tercero)<=100),
+    direc_tercero	        VARCHAR                        NOT NULL       CHECK (LENGTH(direc_tercero) <=4  AND LENGTH(direc_tercero) >=80),
+    long_tercero        	DECIMAL(9,6)                   NOT NULL       CHECK (long_tercero BETWEEN -80   AND -67),
+    lat_tercero         	DECIMAL(9,6)                   NOT NULL       CHECK (lat_tercero  BETWEEN -4    AND 12), 
+    datos_audit           AUDIT_TRAIL,
+    PRIMARY KEY (id_tercero)
+);
+
+--TABLA DE VALOR DEL INDICADOR
+
+CREATE TABLE IF NOT EXISTS tab_val_ind
+(
+    id_val_ind	            VARCHAR                        NOT NULL       CHECK (LENGTH (id_val_ind) >=0            AND     LENGTH (id_val_ind) <=100 ) ,
+    id_pmtro_financiero	    VARCHAR                        NOT NULL       CHECK (LENGTH(id_pmtro_financiero)>=0     AND     LENGTH(id_pmtro_financiero)<=20),
+    id_ind	                VARCHAR                        NOT NULL       CHECK (LENGTH (id_ind) <=50),
+    val_calculado	        DECIMAL  (10,2)                NOT NULL       CHECK (val_calculado >=0),
+    fec_calculo	            DATE                           NOT NULL,       --CHECK ( <> NOW()),
+    datos_audit           AUDIT_TRAIL,
+    PRIMARY KEY (id_val_ind),
+    FOREIGN KEY (id_pmtro_financiero)     REFERENCES tab_pmtros_financieros(id_pmtro_financiero),
+    FOREIGN KEY (id_ind)                 REFERENCES tab_indicadores(id_ind)
+);
